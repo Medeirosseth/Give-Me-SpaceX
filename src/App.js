b9f89d6ef601c87a097ae42f12d64a44b9afc5a7
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-10-12 16:37:30 -0700

    add successful api calls

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import './App.css';
+import React from 'react';
+import { useQuery, gql } from '@apollo/client'
+
+const ROCKETS = gql`
+{
+  launchesPast {
+    id
+    mission_name
+    rocket {
+      rocket_name
+      rocket {
+        active
+      }
+    }
+  }
+}
+`
 
 function App() {
-  return (
-    <div className="App">
 
+  const { loading, error, data } = useQuery(ROCKETS)
+  if(loading) return <p>'Tis loading...</p>
+  if(error) return <p>Elon, we have a problem</p>
+  return (
+    <>
+    <div className="App">
+      <h2>SPACE STUFF</h2>
+      {data.launchesPast.map((launchesPast, id) => (
+        <p key={id}>{launchesPast.mission_name}</p>
+      ))}
     </div>
+    </>
   );
 }
 
